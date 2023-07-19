7c8d007cdf94a1823d0925794bcca435142f716f
dophison <77040766+dophison@users.noreply.github.com>
2023-07-19 23:22:17 +0700

    check when not sign in

--- pythonProject1/test.py
+++ pythonProject1/test.py
@@ -1,234 +1,69 @@
-import  requests
-from flask import Flask, request, jsonify, json
-import urllib.request
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
 
 
-import urllib.parse
-app = Flask(__name__)
-# cookie_json = """
-# [
-#     {
-#         "domain": ".facebook.com",
-#         "expirationDate": 1724033786.268623,
-#         "hostOnly": false,
-#         "httpOnly": true,
-#         "name": "datr",
-#         "path": "/",
-#         "sameSite": "no_restriction",
-#         "secure": true,
-#         "session": false,
-#         "storeId": null,
-#         "value": "fVMHZPVsvIum8AWajr7hvGG8"
-#     },
-#     {
-#         "domain": ".facebook.com",
-#         "expirationDate": 1720969492,
-#         "hostOnly": false,
-#         "httpOnly": false,
-#         "name": "fbl_ci",
-#         "path": "/",
-#         "sameSite": "no_restriction",
-#         "secure": true,
-#         "session": false,
-#         "storeId": null,
-#         "value": "988100872341208"
-#     },
-#     {
-#         "domain": ".facebook.com",
-#         "expirationDate": 1697520799.799278,
-#         "hostOnly": false,
-#         "httpOnly": true,
-#         "name": "fr",
-#         "path": "/",
-#         "sameSite": "no_restriction",
-#         "secure": true,
-#         "session": false,
-#         "storeId": null,
-#         "value": "0vw96Fn1BTWbCA6PF.AWXHfoniOH4q-SC9ZRJ4Kvxurl4.Bkt3Wh.SA.AAA.0.0.Bkt3Wh.AWVL-FU-QOQ"
-#     },
-#     {
-#         "domain": ".www.facebook.com",
-#         "expirationDate": 1724304066,
-#         "hostOnly": false,
-#         "httpOnly": false,
-#         "name": "m_ls",
-#         "path": "/",
-#         "sameSite": "no_restriction",
-#         "secure": true,
-#         "session": false,
-#         "storeId": null,
-#         "value": "%7B%22c%22%3A%7B%221%22%3A%22HCwAABaYihQW6KKx2w0TBRaS-5e5ir8tAA%22%2C%222%22%3A%22GSwVQBxMAAAWRhbk1eDKDBYAABV-HEwAABaCAhbq1eDKDBYAABYoAA%22%2C%2295%22%3A%22HCwAABbGBBaetZD1CxMFFpL7l7mKvy0A%22%7D%2C%22d%22%3A%22d145873d-e826-44aa-8f48-ea5b635ce9e4%22%2C%22s%22%3A%221%22%2C%22u%22%3A%22ywmwc8%22%7D"
-#     },
-#     {
-#         "domain": ".facebook.com",
-#         "expirationDate": 1721280800.799265,
-#         "hostOnly": false,
-#         "httpOnly": true,
-#         "name": "xs",
-#         "path": "/",
-#         "sameSite": "no_restriction",
-#         "secure": true,
-#         "session": false,
-#         "storeId": null,
-#         "value": "7%3ADYNAMiFLjB9pmw%3A2%3A1678209186%3A-1%3A14929%3A%3AAcVfQhCh31nZ0yq_z7V4q-RKxZt2GBHQhGV2clN_O7Dw"
-#     },
-#     {
-#         "domain": ".facebook.com",
-#         "expirationDate": 1720969492,
-#         "hostOnly": false,
-#         "httpOnly": false,
-#         "name": "fbl_st",
-#         "path": "/",
-#         "sameSite": "strict",
-#         "secure": true,
-#         "session": false,
-#         "storeId": null,
-#         "value": "100434730%3BT%3A28157224"
-#     },
-#     {
-#         "domain": ".facebook.com",
-#         "expirationDate": 1690038291.96528,
-#         "hostOnly": false,
-#         "httpOnly": false,
-#         "name": "locale",
-#         "path": "/",
-#         "sameSite": "no_restriction",
-#         "secure": true,
-#         "session": false,
-#         "storeId": null,
-#         "value": "vi_VN"
-#     },
-#     {
-#         "domain": ".facebook.com",
-#         "expirationDate": 1721280800.799217,
-#         "hostOnly": false,
-#         "httpOnly": false,
-#         "name": "c_user",
-#         "path": "/",
-#         "sameSite": "no_restriction",
-#         "secure": true,
-#         "session": false,
-#         "storeId": null,
-#         "value": "100039780400841"
-#     },
-#     {
-#         "domain": ".facebook.com",
-#         "hostOnly": false,
-#         "httpOnly": false,
-#         "name": "presence",
-#         "path": "/",
-#         "sameSite": null,
-#         "secure": true,
-#         "session": true,
-#         "storeId": null,
-#         "value": "C%7B%22t3%22%3A%5B%5D%2C%22utc3%22%3A1689744088561%2C%22v%22%3A1%7D"
-#     },
-#     {
-#         "domain": ".facebook.com",
-#         "expirationDate": 1690348891,
-#         "hostOnly": false,
-#         "httpOnly": false,
-#         "name": "dpr",
-#         "path": "/",
-#         "sameSite": "no_restriction",
-#         "secure": true,
-#         "session": false,
-#         "storeId": null,
-#         "value": "0.8999999761581421"
-#     },
-#     {
-#         "domain": ".facebook.com",
-#         "expirationDate": 1720969492,
-#         "hostOnly": false,
-#         "httpOnly": false,
-#         "name": "fbl_cs",
-#         "path": "/",
-#         "sameSite": "no_restriction",
-#         "secure": true,
-#         "session": false,
-#         "storeId": null,
-#         "value": "AhDpAnD8XOY%2BQ1mdFTCpIT8gGE1laHBxQmdjbGw2cmJWK2tOc1dyeUltaw"
-#     },
-#     {
-#         "domain": ".facebook.com",
-#         "expirationDate": 1697217896.73501,
-#         "hostOnly": false,
-#         "httpOnly": true,
-#         "name": "m_page_voice",
-#         "path": "/",
-#         "sameSite": "no_restriction",
-#         "secure": true,
-#         "session": false,
-#         "storeId": null,
-#         "value": "100039780400841"
-#     },
-#     {
-#         "domain": ".facebook.com",
-#         "expirationDate": 1712769188.565503,
-#         "hostOnly": false,
-#         "httpOnly": true,
-#         "name": "sb",
-#         "path": "/",
-#         "sameSite": "no_restriction",
-#         "secure": true,
-#         "session": false,
-#         "storeId": null,
-#         "value": "fVMHZKbd29ShKlbgk9C57LkB"
-#     },
-#     {
-#         "domain": ".facebook.com",
-#         "expirationDate": 1690348888,
-#         "hostOnly": false,
-#         "httpOnly": false,
-#         "name": "wd",
-#         "path": "/",
-#         "sameSite": "lax",
-#         "secure": true,
-#         "session": false,
-#         "storeId": null,
-#         "value": "1728x912"
-#     },
-#     {
-#         "domain": ".facebook.com",
-#         "expirationDate": 1697209492,
-#         "hostOnly": false,
-#         "httpOnly": false,
-#         "name": "wl_cbv",
-#         "path": "/",
-#         "sameSite": "no_restriction",
-#         "secure": true,
-#         "session": false,
-#         "storeId": null,
-#         "value": "v2%3Bclient_version%3A2288%3Btimestamp%3A1689433491"
-#     }
-# ]
-# """
-#
-# # cookies = {
-# #     "datr": "wZytZJ4b0WxLNY0zfsMeEQEW",
-# #     "fr" : "0FcrbPUqDxLrE4pRf.AWX7bJ0arCiSjVLxF_22v39R06U.BktogL.0g.AAA.0.0.BktpV1.AWX_J23UJvI",
-# #     "xs" : "40%3AfmcgtJoVddrtkw%3A2%3A1689099584%3A-1%3A13332%3A%3AAcWTtJ11-eeU-7_AkXJnrYETwPMnVF_8lMURg6rvTks",
-# #     "fr" : "0FcrbPUqDxLrE4pRf.AWX7bJ0arCiSjVLxF_22v39R06U.BktogL.0g.AAA.0.0.BktpV1.AWX_J23UJvI",
-# #     "fr" : "0FcrbPUqDxLrE4pRf.AWX7bJ0arCiSjVLxF_22v39R06U.BktogL.0g.AAA.0.0.BktpV1.AWX_J23UJvI",
-# #     "fr" : "0FcrbPUqDxLrE4pRf.AWX7bJ0arCiSjVLxF_22v39R06U.BktogL.0g.AAA.0.0.BktpV1.AWX_J23UJvI",
-# #     "fr" : "0FcrbPUqDxLrE4pRf.AWX7bJ0arCiSjVLxF_22v39R06U.BktogL.0g.AAA.0.0.BktpV1.AWX_J23UJvI",
-# #     "fr" : "0FcrbPUqDxLrE4pRf.AWX7bJ0arCiSjVLxF_22v39R06U.BktogL.0g.AAA.0.0.BktpV1.AWX_J23UJvI",
-# #     "fr" : "0FcrbPUqDxLrE4pRf.AWX7bJ0arCiSjVLxF_22v39R06U.BktogL.0g.AAA.0.0.BktpV1.AWX_J23UJvI"
-# #
-# # }
-#
-# def convert_json_to_cookie(cookie_json):
-#     # Chuyển đổi đoạn mã JSON thành dạng cookie của requests
-#     cookies = {}
-#     for cookie in json.loads(cookie_json):
-#         cookies[cookie["name"]] = cookie["value"]
-#     return cookies
-#
-#     # Chuyển đoạn mã JSON thành dạng cookie của requests
-# cookies = convert_json_to_cookie(cookie_json)
 
-    # Thực hiện yêu cầu GET với các cookie đã có
-response = requests.get("https://www.facebook.com/profile.php?id=100090937081290&__tn__=-UK*F")
-# response = urllib.request.urlopen('')
-# html_content = response.read().decode('utf-8')
-print(response.text)
+
+# Khởi tạo trình duyệt Firefox
+driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+try:
+    # Mở trang đăng nhập của Facebook
+    driver.get("https://www.facebook.com/login")
+
+    # Nhập thông tin đăng nhập (email hoặc số điện thoại và mật khẩu)
+    email_or_phone = "van1973cute@gmail.com"
+    password = "haudinhtruong20122002"
+
+
+
+    email_phone_input = driver.find_element(By.ID,"email")
+    email_phone_input.send_keys(email_or_phone)
+    password_input = driver.find_element(By.ID,"pass")
+    password_input.send_keys(password)
+
+    #Thực hiện đăng nhập bằng cách nhấn Enter
+
+    password_input.send_keys(Keys.ENTER)
+
+    # Đợi một lúc để trang đăng nhập xử lý
+    time.sleep(5)
+    url = "https://www.facebook.com/quanganhdeptraivclluon"
+    driver.get(url)
+    response = driver.page_source
+    if '<link rel="alternate" hreflang="x-default" href="https://www.facebook.com/' in response:
+        href_start = response.find('<link rel="alternate" hreflang="x-default" href="') + len(
+            '<link rel="alternate" hreflang="x-default" href="')
+        href_end = response.find('"', href_start)
+        href_content = response[href_start:href_end]
+
+        style = 'green'
+        print(style)
+        # Kiểm tra trong response có <meta name="description" content=" hay không
+        if '<meta name="description" content="' in response:
+            meta_start = response.find('<meta name="description" content="') + len(
+                '<meta name="description" content="')
+            meta_end = response.find('&#x111', meta_start)
+            name = response[meta_start:meta_end]
+        else:
+            name = ''
+    elif '<id="facebook" class="no_js">' in response:
+            # Trả về style màu đỏ
+        style = 'red'
+        print(style)
+
+
+
+
+
+
+
+except Exception as e:
+    print(f"Failed to log in to Facebook: {e}")
+
+finally:
+    # Đóng trình duyệt sau khi hoàn thành
+    driver.quit()
--- pythonProject1/web_checkstatus.py
+++ pythonProject1/web_checkstatus.py
@@ -3,22 +3,19 @@ from flask import Flask
 import urllib.parse
 import re
 import html
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
+
+
 app = Flask(__name__)
-
-
 def read_file_content():
     with open('checkscam.txt', 'r', encoding='utf-8') as file:
         content = file.readlines()
     return content
-
-
-@app.route('/')
-def home():
-    content = read_file_content()
-    formatted_content = '<br>'.join(f"{index + 1}. {line}" for index, line in enumerate(content))
-    return f"DANH SÁCH LINK FACEBOOK SCAM<br>{formatted_content}"
-
-
 def read_debug_links():
     with open('debug_link.txt', 'r', encoding='utf-8') as file:
         links = file.readlines()
@@ -30,31 +27,32 @@ def convert_percent_encoded_to_unicode(text):
     return decoded_text
 
 
-def check_link_status(link):
+def check_link_status(response):
     try:
-        response = requests.get(link)
-        if '<link rel="alternate" hreflang="x-default" href="https://www.facebook.com/' in response.text:
-            href_start = response.text.find('<link rel="alternate" hreflang="x-default" href="') + len(
+        if '<link rel="alternate" hreflang="x-default" href="https://www.facebook.com/' in response:
+            href_start = response.find('<link rel="alternate" hreflang="x-default" href="') + len(
                 '<link rel="alternate" hreflang="x-default" href="')
-            href_end = response.text.find('"', href_start)
-            href_content = response.text[href_start:href_end]
+            href_end = response.find('"', href_start)
+            href_content = response[href_start:href_end]
             style = 'green'
-            # Kiểm tra trong response.text có <meta name="description" content=" hay không
-            if '<meta name="description" content="' in response.text:
-                meta_start = response.text.find('<meta name="description" content="') + len(
+            # Kiểm tra trong response có <meta name="description" content=" hay không
+            if '<meta name="description" content="' in response:
+                meta_start = response.find('<meta name="description" content="') + len(
                     '<meta name="description" content="')
-                meta_end = response.text.find('&#x111', meta_start)
-                name = response.text[meta_start:meta_end]
+                meta_end = response.find('đang', meta_start)
+                name = response[meta_start:meta_end]
+                print(name)
             else:
                 name = ''
             return href_content, name, style
-        elif '<html lang="vi" id="facebook" class="no_js">' in response.text:
+        elif '<id="facebook" class="no_js">' in response:
+
             # Trả về style màu đỏ
             style = 'red'
             return '','', style
-        return ''
+        return '','','red'
     except requests.exceptions.RequestException:
-        return ''
+        return '','','red'
 
 
 # Lấy name từ trong status có dạng https://www.facebook.com/people/abcxsttat\
@@ -69,27 +67,57 @@ def get_name_from_status(status):
     return name
 
 
+@app.route('/')
+def home():
+    content = read_file_content()
+    formatted_content = '<br>'.join(f"{index + 1}. {line}" for index, line in enumerate(content))
+    return f"DANH SÁCH LINK FACEBOOK SCAM<br>{formatted_content}"
+
 @app.route('/status')
 def status():
+
     debug_links = read_debug_links()
+    driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
     result = []
-    for link in debug_links:
-        status, name, style = check_link_status(link)
-        print(status)
-        if status.startswith('https://www.facebook.com/'):
-            name = convert_percent_encoded_to_unicode(name)
-            result.append({'status': status, 'name': name, 'style': style})
-        if status == '':
-            status = link
-            result.append({'status': status, 'name': "Unknown", 'style': style})
+    # Mở trang đăng nhập của Facebook
+    driver.get("https://www.facebook.com/login")
+
+    # Nhập thông tin đăng nhập (email hoặc số điện thoại và mật khẩu)
+    email_or_phone = "van1973cute@gmail.com"
+    password = "haudinhtruong20122002"
+    email_phone_input = driver.find_element(By.ID,"email")
+    email_phone_input.send_keys(email_or_phone)
+    password_input = driver.find_element(By.ID,"pass")
+    password_input.send_keys(password)
+    # Thực hiện đăng nhập bằng cách nhấn Enter
+    password_input.send_keys(Keys.ENTER)
+
+    try:
+        for link in debug_links:
+            driver.get(link)
+            response = driver.page_source
+            status, name, style = check_link_status(response)
+            if status.startswith('https://www.facebook.com/'):
+                name = convert_percent_encoded_to_unicode(name)
+                result.append({'status': status, 'name': name, 'style': style})
+            if status == '':
+                status = link
+                result.append({'status': status, 'name': "Unknown", 'style': style})
+    finally:
+        driver.quit()
     formatted_content = '<br>'.join(
-        f'<span style="color: {item["style"]}">{index + 1}. {item["name"]} ----> {item["status"]}  </span>'
+        f'<span style="color: {item["style"]}">{index + 1}.{item["name"]} ----> {item["status"]}  </span>'
         for index, item in enumerate(result)
     )
+    with open('history.html', 'w', encoding='utf-8') as file:
+        file.write("THÔNG TIN VỀ TÌNH TRẠNG HOẠT ĐỘNG")
+        file.write(formatted_content)
+
     return f"THÔNG TIN VỀ TÌNH TRẠNG HOẠT ĐỘNG<br>{formatted_content}"
 
 
+
 if __name__ == '__main__':
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=5000)
+    # from waitress import serve
+    # serve(app, host="0.0.0.0", port=5000)
     app.run(debug=True)
